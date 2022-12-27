16c663fc433dfcae6f0e8338430635ac3e2829ef
Cahuete0512 <magalie.contant@epsi.fr>
2022-12-27 17:43:52 +0100

    Adding a footer with false mentions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <nav-bar/>
+  <Footer/>
 </template>
 <script>
 
 import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: 'App',
-  components: {NavBar}
+  components: {
+    NavBar,
+    Footer
+  }
 }
 </script>
 
 <style>
 @import url('../src/style/RandomStyle.css');
 @import url('../src/style/NavBarStyle.css');
+@import url('../src/style/Footer.css');
+
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div id="mentions_légales">
+      <ul>
+        <p><b> Créatrices :</b> Eline Malherbe, Magalie Contant et Ines Boucard  </p>
+        <p>©2023-EMI.COCKTAIL-L'abus d'alcool est dangereux pour la santé</p>
+        <p>Politique de confidentialité</p>
+        <p>A propos de EMI.COCKTAIL</p>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterSection',
+};
+</script>
