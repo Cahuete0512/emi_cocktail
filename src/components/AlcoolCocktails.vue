4994b6cca4bb017ea506cf753415ef3ecd64df36
ines-boucard <72189183+ines-boucard@users.noreply.github.com>
2023-01-10 16:49:00 +0100

    vue fonctionnelle avec le css inclut dans le fichier

--- src/components/AlcoolCocktails.vue
+++ src/components/AlcoolCocktails.vue
@@ -1,16 +1,81 @@
 <template>
-  <div class="alcoolCocktails">
-    <p>
-      Cocktails avec alcool ...
-    </p>
+  <div id="demo">
+    <div class="containerCocktail">
+      <div v-for="cocktail in cocktails.drinks" :key="cocktail" class="cocktail" >
+        <!-- <p>{{cocktail.strDrink}}</p>-->
+        <img class="image" v-bind:src="cocktail.strDrinkThumb" title="cliquez pour avoir la recette">
+
+
+      </div>
+    </div>
   </div>
 </template>
+
+
+
+
+
+<!--<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="https://unpkg.com/vue@3"></script>-->
+
+
+/*export default {
+
+}*/
 <script>
+import axios from 'axios'
+const constapiURL=
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic";
+
+
 export default {
-  name: 'AlcoolCocktails'
-}
+  /*name: 'AlcoolCocktails',*/
+  components: {},
+  data() {
+    return {
+      cocktails: {},
+    };
+  },
+  created: function () {
+    this.fetchDataPromise()
+  },
+  methods: {
+    fetchDataPromise: function () {
+     const self = this;
+      axios
+          .get(constapiURL)
+          .then(function (response) {
+            console.log(response.data);
+            self.cocktails = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    },
+  },
+
+};
+
 </script>
 
 <style>
+#demo {
+  font-family: "Helvetica", Arial, sans-serif;
+}
+.containerCocktail {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  border-radius: 200px;
+}
 
+.image{
+  width: 500px;
+  height: 500px;
+ /* border-radius: 50%;*/
+  margin: 50px;
+}
+.image:hover{
+  filter: grayscale(100%);
+}
 </style>
