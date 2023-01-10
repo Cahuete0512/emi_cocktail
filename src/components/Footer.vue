bdc692de876e6520debbea88b779931b7a4df415
Cahuete0512 <magalie.contant@epsi.fr>
2023-01-10 16:05:49 +0100

    Modification on project architecture

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer>
-        <p><b> Créatrices :</b> Eline Malherbe, Magalie Contant et Ines Boucard  </p>
-        <p>©2023-EMI.COCKTAIL-L'abus d'alcool est dangereux pour la santé</p>
-        <p>Politique de confidentialité</p>
-        <p>A propos de EMI.COCKTAIL</p>
+    <p><b> Créatrices :</b> Eline Malherbe, Magalie Contant et Ines Boucard </p>
+    <p>©2023-EMI.COCKTAIL-L'abus d'alcool est dangereux pour la santé</p>
+    <p>Politique de confidentialité</p>
+    <p>A propos de EMI.COCKTAIL</p>
   </footer>
 </template>
 
